f6ed8b602f38fa567b6043e422c2776100abefde
jefftdb <jefftdb@gmail.com>
2024-08-19 00:48:33 -0300

    Consegui resolver a 5

--- Tarefa5.js
+++ Tarefa5.js
@@ -1,38 +1,33 @@
-const array1 = [
-    { id: 1, name: 'Alice', age: 25 },
-    { id: 2, name: 'Bob', age: 30 }
-  ];
-  
-  const array2 = [
-    { id: 1, age: 26, email: 'alice@example.com' },
-    { id: 3, name: 'Charlie', age: 22 }
-  ];
-  const arrayconcat=[];
-  const todosArray = array1.concat(array2);
 
+function mergeArrays(array1,array2){
+    
+
+  const arrayconcat=[];
+   
   
-  
-  for (const arr1 of todosArray){
-      if(todosArray[todosArray.indexOf(arr1 + 1)]== undefined){
+  for (const arr1 of array1){      
           arrayconcat.push(arr1);
       }
 
- for (let arr2 of arrayconcat) {
-      for (let arr3 of todosArray) {
-          if(arr2.id == arr3.id){
-             const unir = Object.assign(arr2,arr3);
+ for (const arrc of arrayconcat) {
+      for (const arr2 of array2) {
+          if(arrc.id == arr2.id){
+             const unir = Object.assign(arrc,arr2);
               
-              console.log(arrayconcat.indexOf(arr3));
+          }else{
+              const idJaExiste = arrayconcat.find((arrayconcat)=>{
+                  return arrayconcat.id == arr2.id
+              })
+
+              if(idJaExiste){
+              }
+              else{
+                arrayconcat.push(arr2);  
+              }
+              
           }
          
      }
- }         
-         
-        
-  }
-      
-  
-console.log(arrayconcat);
-
-  
-  //So falta conseguir apagar apenas o certo
+ }
+    return arrayconcat
+}
